1bdaf3e593bc3bc42eb04911a2952ec37e5d8017
souzapvtd <oliveira.souza.miguel@escola.pr.gov.br>
2026-02-11 09:42:00 -0300

    Add files via upload

--- imc.js
+++ imc.js
@@ -1,1 +1,39 @@
 const calcular = document.getElementById('calcular');
+
+function imc(){
+const nome = document.getElementById ('nome').value;
+const altura = document.getElementById ('altura').value;
+const peso = document.getElementById ('peso').value;
+const resultado = document.getElementById('resultado');
+
+
+if(nome !==''&& altura !==''&& peso !==''){
+const valorIMC=(peso/(altura*altura)).toFixed (1);
+let classificacao = '';
+
+if(valorIMC <18.5){
+classificacao = 'abaixo do peso';
+
+   }else if(valorIMC <25){
+        classificacao = 'com peso normal';
+
+   }else if(valorIMC <30){
+        classificacao = 'levemente acima do peso'
+
+   }else if(valorIMC <35){
+         classificacao = 'obesidade grau 1'
+
+   }else if(valorIMC <40){
+        classificacao = 'obesidade grau 2'
+
+   }else {
+         classificacao = 'atenção'
+}
+resultado.textContent = `${nome} seu imc é ${valorIMC} e você está ${classificacao}`;
+
+}
+else{
+    resultado.textContent = 'preencha todos os campos'
+}
+}
+calcular.addEventListener ('click', imc);
